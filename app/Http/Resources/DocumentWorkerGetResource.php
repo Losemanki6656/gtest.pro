440fa078a159a2162225a144f033fca9079e11f0
Losemanki6656 <bbiznestg@gmail.com>
2023-02-23 18:01:30 +0500

    sad

--- app/Http/Resources/DocumentWorkerGetResource.php
+++ app/Http/Resources/DocumentWorkerGetResource.php
@@ -14,6 +14,9 @@ class DocumentWorkerGetResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'photo' => url(asset($this->photo))
+        ];
     }
 }
